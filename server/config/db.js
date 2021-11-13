b7fb6da56716447cedaacdcd382c472cc92133a2
iritsanna <iritsannamathew@gmail.com>
2021-11-14 05:11:02 +0530

    Survey List Page

--- server/config/db.js
+++ server/config/db.js
@@ -2,4 +2,5 @@
 // DB configurations here
 module.exports = {
     // "URI": "mongodb://localhost/user"
+ "URI":   "mongodb+srv://irits:dtAmtipWrElA4jEW@mongodbserver.xnnlf.mongodb.net/users?retryWrites=true&w=majority"
 }
